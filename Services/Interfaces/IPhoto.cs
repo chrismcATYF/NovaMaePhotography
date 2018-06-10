662b87d43190d0c8efe147d72ce5eaf3bbe9c781
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2018-06-10 16:47:57 -0500

    Added Context DB

---
+++ Services/Interfaces/IPhoto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NovaMaePhotography.Models;
+
+namespace NovaMaePhotography.Services.Interfaces
+{
+    public interface IPhoto
+    {
+        List<Photos> GetAllPhotos();
+        Photos GetPhoto(int id);
+
+    }
+}
---
+++ Services/PhotoService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NovaMaePhotography.Models;
+using NovaMaePhotography.Services;
+using NovaMaePhotography.Services.Interfaces;
+
+namespace NovaMaePhotography.Services
+{
+    public class PhotoService : IPhoto
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public PhotoService()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public List<Photos> GetAllPhotos()
+        {
+            return _context.Photos.ToList();
+        }
+
+        public Photos GetPhoto(int id)
+        {
+            return _context.Photos.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
